ad576d29b82d7f432b60f0d3d72a02cb27f8cc48
Yurii Zhurakovskyi <yuriizhurakovskyi7@gmail.com>
2020-06-26 20:59:03 +0300

    add contextLoads test

--- src/test/java/ua/lviv/yurii/zhurakovskyi/my/selection/committee/MySelectionCommitteeApplicationTests.java
+++ src/test/java/ua/lviv/yurii/zhurakovskyi/my/selection/committee/MySelectionCommitteeApplicationTests.java
@@ -3,8 +3,8 @@ package ua.lviv.yurii.zhurakovskyi.my.selection.committee;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import ua.lviv.yurii.zhurakovskyi.my.selection.committee.dao.UserRepository;
 import ua.lviv.yurii.zhurakovskyi.my.selection.committee.domain.User;
@@ -14,11 +14,12 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ua.lviv.yurii.zhurakovskyi.my.selection.committee.domain.Role.ROLE_USER;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-@SpringBootTest
+@AutoConfigureTestDatabase
 class MySelectionCommitteeApplicationTests {
 
     @Autowired
@@ -42,6 +43,12 @@ class MySelectionCommitteeApplicationTests {
         userService.save(user);
         users = userRepository.findAll();
         assertThat(users, hasSize(1));
+        User userFromDB = users.get(0);
+
+        assertEquals(userFromDB.getEmail(), user.getEmail());
+        assertEquals(userFromDB.getFirstName(), user.getFirstName());
+        assertEquals(userFromDB.getLastName(), user.getLastName());
+        assertEquals(userFromDB.getRole(), user.getRole());
     }
 
 }
